61ec1ad9101919bfa48e72d22ce20be01b19c9a2
miguelrochaxavier <miguellrochaxavier@gmail.com>
2025-03-06 15:54:12 -0300

    feat: adição da função de calcular fatorial simples

--- main.py
+++ main.py
@@ -13,9 +13,11 @@ print('Calculadora de fatorial')
 print('1- Fatorial simples')
 print('2- Fatorial dupla')
 print('3- Fatorial tripla')
-esc = int(input('#3'))
+esc = int(input('#'))
 
 if esc == 1 :
     x = int(input('Digite seu numero aqui : '))
     print(f'O fatorial de {x}! é {fatorial(x)}')
 
+
+# Colocar calculadora de fração com uma icognita divindo por partes, k = parte de cima da fração & l = parte de baixo da fração

b26f3ddcf4980b6d1a65ec1e3ca4f24da3c624dc
miguelrochaxavier <miguellrochaxavier@gmail.com>
2025-03-07 16:29:15 -0300

    fix : arrumando o app

--- app.py
+++ app.py
@@ -1,9 +1,1 @@
-from kivy.app import App  
-from kivy.uix.button import Button  
-
-class MeuApp(App):  
-    def build(self):  
-        return Button(text='Hello World!')  
-
-if __name__ == '__main__':  
-    MeuApp().run()  
+from customtkinter import *

f2647303980d2df82d82103843bd0e7368454131
miguelrochaxavier <miguellrochaxavier@gmail.com>
2025-03-07 08:28:29 -0300

    fix : resolução do bug da def fatorial

--- main.py
+++ main.py
@@ -3,12 +3,6 @@
 
 import math
 
-def fatorial(n) :
-    if n == 0 or n == 1 :
-        return 1
-    else : 
-        return n * fatorial (n-1)
-
 print('Calculadora de fatorial')
 print('1- Fatorial simples')
 print('2- Fatorial dupla')
@@ -16,6 +10,13 @@ print('3- Fatorial tripla')
 esc = int(input('#'))
 
 if esc == 1 :
+
+    def fatorial(n) :
+        if n == 0 or n == 1 :
+            return 1
+        else : 
+            return n * fatorial (n-1)
+
     x = int(input('Digite seu numero aqui : '))
     print(f'O fatorial de {x}! é {fatorial(x)}')
 

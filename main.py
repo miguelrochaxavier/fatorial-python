f9e9b52dde7a90f4502a85538a118e2dc1f1c7a8
miguelrochaxavier <miguellrochaxavier@gmail.com>
2025-03-07 19:30:20 -0300

    fix : corrigir a soma fatoriais

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ print('Calculadora de fatorial')
 print('1- Fatorial simples')
 print('2- Fatorial dupla')
 print('3- Fatorial tripla')
+print('4- Soma de fatoriais')
 esc = int(input('#'))
 
 if esc == 1 :
@@ -39,6 +40,28 @@ elif esc == 3 :
         
     x = int(input('Digite seu numero aqui : '))
     print(f'O fatorial de {x}!!! é {fatorialTripla(x)}')
+
+elif esc == 4 :
+
+    def somaFatoriaisk(n) :
+        if n == 0 or n == 1 :
+            return 1
+        else :
+            return n * somaFatoriaisk (n-1)
+        
+    def somaFatoriaiskk(nk) :
+        if nk == 0 or nk == 1 :
+            return 1
+        else :
+            return nk * somaFatoriaiskk (nk-1)
+        
+    def somaTotal(n, nk) :
+        nk + n = nk
+        return nk
+
+    x = int(input('Digite seu numero aqui : '))
+    j = int(input('Digite seu numero novamente aqui : '))
+    print(f'O fatorial de {nk}!!! é {somaTotal(nk)}')
 
 else :
     print('Erro número não encontrado. Tente novamente!')

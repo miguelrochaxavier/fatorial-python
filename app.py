2a2e3767944dadb266c6efeb0fe520231571ba37
miguelrochaxavier <miguellrochaxavier@gmail.com>
2025-03-07 17:28:30 -0300

    fix : def's

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 from customtkinter import *
-from PIL import Image
+
+def fatorial_number():
+    global x
+    x = entry.get()  
+    try:
+        x = int(x)  
+        print(f'Valor salvo: {x}')
+    except ValueError:
+        print('Error! Insira um número válido')
+
+def fatorial_select(value):
+    print(f'Selecionado {value}')
 
 app = CTk()
 app.geometry('600x500')
@@ -9,9 +20,6 @@ app.title('Calculadora Fatorial com Python e Tkinter')
 CTkLabel(master=app, text="Calculadora Fatorial com Python e Tkinter", font=("Arial Bold", 20), justify="left").pack(anchor="w", pady=(43, 18), padx=(56,0))
 stats_frame = CTkFrame(master=app, fg_color="transparent")
 stats_frame.pack( padx=(54, 0), pady=(18, 0), anchor="nw")
-
-def change_handle(value):
-    print(f'Selecionado {value}')
 
 btn = CTkComboBox(master=app, values=['1-Fatorial Simples', '2-Fatorial Dupla', '3-Fatorial Tripla'],)
 btn.place(relx=0.2, rely=0.3, anchor='center')

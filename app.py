d1a119b40749a530631a8fc3fcf03997231da16d
miguelrochaxavier <miguellrochaxavier@gmail.com>
2025-03-07 17:42:31 -0300

    update

--- app.py
+++ app.py
@@ -1,16 +1,44 @@
 from customtkinter import *
+import math
+
+def fatorial_select(choice):
+    global tipo_fatorial
+    tipo_fatorial = int(choice.split('-')[0])
 
 def fatorial_number():
-    global x
-    x = entry.get()  
     try:
-        x = int(x)  
-        print(f'Valor salvo: {x}')
+        n = int(entry.get())  
+        if tipo_fatorial == 1:
+            result = fatorial(n)
+            resultado_label.configure(text=f'O fatorial de {n}! é {result}')
+        elif tipo_fatorial == 2:
+            result = fatorialDuplo(n)
+            resultado_label.configure(text=f'O fatorial duplo de {n}!! é {result}')
+        elif tipo_fatorial == 3:
+            result = fatorialTripla(n)
+            resultado_label.configure(text=f'O fatorial triplo de {n}!!! é {result}')
+        else:
+            resultado_label.configure(text='Erro: Tipo de fatorial não encontrado.')
     except ValueError:
-        print('Error! Insira um número válido')
+        resultado_label.configure(text='Erro: Entrada inválida. Digite um número inteiro.')
 
-def fatorial_select(value):
-    print(f'Selecionado {value}')
+def fatorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * fatorial(n-1)
+
+def fatorialDuplo(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * fatorialDuplo(n-2)
+
+def fatorialTripla(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * fatorialTripla(n-3)
 
 app = CTk()
 app.geometry('600x500')
@@ -29,5 +57,8 @@ entry.place(relx=0.33, rely=0.4, anchor='center')
 
 btn = CTkButton(master=app, text='Clique aqui', command=fatorial_number)
 btn.place(relx=0.2, rely=0.5, anchor='center')
+
+resultado_label = CTkLabel(master=app, text="", font=("Arial", 14))
+resultado_label.place(relx=0.5, rely=0.6, anchor='center')
 
 app.mainloop()

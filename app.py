be888bd4989d5b6f39f0e438159e8082f3ca20c0
miguelrochaxavier <miguellrochaxavier@gmail.com>
2025-03-07 16:37:02 -0300

    update : app

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-from customtkinter import *
+import customtkinter as ctk
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("blue")
+
+app = ctk.CTk()
+app.title("Teste CustomTkinter")
+app.geometry("400x300")
+
+label = ctk.CTkLabel(app, text="Olá, CustomTkinter!")
+label.pack(pady=20)
+
+app.mainloop()
